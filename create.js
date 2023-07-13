f22fdc73ecf137626e27d6b64e6c369d188f0c14
CMICHAELV <mikec4dmasses@gmail.com>
2023-07-13 04:54:27 -0700

    second commit

---
+++ create.js
@@ -0,0 +1,16 @@
+const person = [{firstName:"paul",lastName:"james",age:46}];
+
+let firstnum = 10;
+let Secondnum = 5; 
+let isgreater = firstnum > Secondnum
+console; log (isgreater); [true]
+
+const name = [{firstName:"brown",lastName:"bright",age:50,eyecolor:"blue"},[1,2,3,4,5,10,]]
+
+const car =["toyota","audi","benz","mazda","volvo"]
+
+[true,false] 
+
+Array.length =(5)
+
+Array.push(5)
